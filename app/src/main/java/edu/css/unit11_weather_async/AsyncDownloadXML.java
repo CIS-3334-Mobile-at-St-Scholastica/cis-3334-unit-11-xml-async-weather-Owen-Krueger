7fc45c68fc9bc5ed496f880397144b00e3d4f599
Owen-Krueger <owk914@gmail.com>
2019-04-07 10:39:34 -0500

    Completed Unit 11 Assignment

--- app/src/main/java/edu/css/unit11_weather_async/AsyncDownloadXML.java
+++ app/src/main/java/edu/css/unit11_weather_async/AsyncDownloadXML.java
@@ -30,19 +30,28 @@ public class AsyncDownloadXML extends AsyncTask<MainActivity, String, String> {
             // Save a pointer to the main Weather Activity which is passed in as a parameter
             mainActivityLink = new_actWeather[0];
 
+            //Check if zip code is empty and stop action so it doesn't crash
+            String zipcode = mainActivityLink.getLocation();
+            if(zipcode.isEmpty()){
+                return null;
+            }
             // create the XML Pull Parser
             XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
             XmlPullParser xpp = factory.newPullParser();
 
-            String  weatherStrURL =  "http://api.openweathermap.org/data/2.5/weather?zip=55811,us&appid=5aa6c40803fbb300fe98c6728bdafce7&mode=xml&units=imperial";
+            //URL includes inputted zip code
+            String  weatherStrURL =  "http://api.openweathermap.org/data/2.5/weather?zip=" +
+            zipcode + ",us&appid=5aa6c40803fbb300fe98c6728bdafce7&mode=xml&units=imperial";
             URL weatherURL =  new URL(weatherStrURL);
             InputStream stream = weatherURL.openStream();
             xpp.setInput(stream, null);
             int eventType = xpp.getEventType();
 
+            //Strings to output info to user
             String tempStr = "Updating...";			// Temperature Update String
             String windStr = "Updating...";			// Wind Update String
-            publishProgress(tempStr,windStr);
+            String visStr = "Updating...";          // Visibility Update String
+            publishProgress(tempStr,windStr, visStr);
 
             Log.v("== CIS 3334 ==","AsyncDownloadXML repeat until end of document arrives");
             while (eventType != XmlPullParser.END_DOCUMENT) {
@@ -57,13 +66,22 @@ public class AsyncDownloadXML extends AsyncTask<MainActivity, String, String> {
                         // XML should look like: <speed value="11.41" name="Strong breeze"/>
 
                         // ======= CIS 3334 add code here to process wing speed =======
+                        windStr = xpp.getAttributeValue(null, "value");
+                        Log.v("== CIS 3334 ==", "Speed = " + windStr);
+                        publishProgress(tempStr, windStr,visStr);
 
                     }
                     if (tag.equals("temperature")){
                         // XML should look like: <temperature value="37.47" min="33.8" max="41" unit="fahrenheit"/>
                         tempStr = xpp.getAttributeValue(null, "value");
                         Log.v("== CIS 3334 ==","Temp =" + tempStr);
-                        publishProgress(tempStr,windStr);	// Update the display
+                        publishProgress(tempStr,windStr,visStr);	// Update the display
+                    }
+                    if (tag.equals("visibility")){
+                        //XML should look like: <visibility value="201"/>
+                        visStr = xpp.getAttributeValue(null, "value");
+                        Log.v("== CIS 3334 ==", "Visibility = " + visStr);
+                        publishProgress(tempStr,windStr,visStr);
                     }
                 }
                 eventType = xpp.next();
@@ -85,13 +103,22 @@ public class AsyncDownloadXML extends AsyncTask<MainActivity, String, String> {
         }
     }
 
+    /**
+     * While task is running, publishes progress updates for the users
+     * @param update Multiple strings of updates for user
+     */
     @Override
     protected void onProgressUpdate(String... update) {
         Log.v("== CIS 3334 ==","in onProgressUpdate");
         mainActivityLink.setTemp(update[0]);
         mainActivityLink.setWind(update[1]);
+        mainActivityLink.setVisibility(update[2]);
     }
 
+    /**
+     * After background task is done running, set status on Main Activity
+     * @param result status to send to user
+     */
     @Override
     protected void onPostExecute(String result) {
         Log.v("== CIS 3334 ==", "in onPostExecute");
--- app/src/main/java/edu/css/unit11_weather_async/MainActivity.java
+++ app/src/main/java/edu/css/unit11_weather_async/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.css.unit11_weather_async;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,6 +21,10 @@ public class MainActivity extends AppCompatActivity {
 
     AsyncDownloadXML AsyncWeatherDownloader = new AsyncDownloadXML();
 
+    /**
+     * On creation of app
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,24 +37,66 @@ public class MainActivity extends AppCompatActivity {
         etVis =  (EditText) findViewById(R.id.textVis);
     }
 
+    /**
+     * On button click, get weather from OpenWeatherMapAPI and set fields to content
+     * @param v current view
+     * @throws XmlPullParserException XML has parsing error
+     * @throws URISyntaxException URI syntax is incorrect
+     * @throws IOException Input/Output problem
+     */
     public void btnClick (View v) throws XmlPullParserException, URISyntaxException, IOException {
         // Download the weather asynchronously
-        AsyncWeatherDownloader.execute(this);
+        String zip = etLoc.getText().toString();
+        if(!zip.isEmpty()) {
+            //AsyncWeatherDownloader.execute(this);
+
+            //I added this to prevent an IllegalStateException that's caused by running the same task more than
+            //once. Is there a reason we don't do this? Possibly for speed?
+            if(AsyncWeatherDownloader.getStatus() == AsyncTask.Status.FINISHED) {
+                AsyncWeatherDownloader = new AsyncDownloadXML();
+                AsyncWeatherDownloader.execute(this);
+            }
+            else {
+                AsyncWeatherDownloader.execute(this);
+            }
+        }
+        else {
+            setStatus("Zip Code cannot be empty!");
+        }
     }
 
+    /**
+     * Set temp field to String parameter
+     * @param newTemp temperature to set field
+     */
     public void setTemp(String newTemp) {
         etTemp.setText(newTemp);
     }
 
+    /**
+     * Set wind field to String parameter
+     * @param newWind wind to set field
+     */
     public void setWind(String newWind) {
         etWind.setText(newWind);
     }
 
+    public void setVisibility(String newVis){
+        etVis.setText(newVis);
+    }
+
+    /**
+     * Get locations from etLoc field
+     * @return String location
+     */
     public String getLocation() {
         return etLoc.getText().toString();
     }
 
-
+    /**
+     * Toast status of String parameter
+     * @param newStatus status to toast to user
+     */
     public void setStatus(String newStatus) {
         Toast toast=Toast.makeText(getApplicationContext(), newStatus,Toast.LENGTH_LONG );
         toast.show();
